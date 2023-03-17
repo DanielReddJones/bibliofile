304e5aed6bacf742b651dfd4dc95f28c7bf6e939
Daniel Jones <danielreddjones@gmail.com>
2023-03-16 21:10:15 -0500

    Added ability to read file by line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::prelude::*;
+use std::env;
+
+fn main() -> std::io::Result<()>  {
+    let args: Vec<String> = env::args().collect();
+    
+    let filename = &args[1];
+    
+    let mut file = File::open(filename).expect("cannot open file.");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).expect("unreadable file.");
+
+    println!("{}", contents);
+    Ok(())
 }
