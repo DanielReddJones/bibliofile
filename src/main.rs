461ca2a8848e58aaefb59cf6484e7e88f5f74c36
Daniel Jones <danielreddjones@gmail.com>
2023-03-16 21:58:32 -0500

    Update main.rs
    Added ncurses function

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::fs::File;
 use std::io::prelude::*;
 use std::env;
-use ncurses;
+extern crate ncurses;
 
 fn main() -> std::io::Result<()>  {
     let args: Vec<String> = env::args().collect();
@@ -13,5 +13,18 @@ fn main() -> std::io::Result<()>  {
     file.read_to_string(&mut contents).expect("unreadable file.");
 
     println!("{}", contents);
+    screen();
     Ok(())
+
+    
+}
+
+fn screen(){
+
+    ncurses::initscr();
+    ncurses::printw("Hello World!");
+    ncurses::refresh();
+    ncurses::getch();
+    ncurses::endwin();
+
 }
