7e626d1c294196fc924c556517cdba1ebbeb7e85
Daniel Redd Jones <danielreddjones@gmail.com>
2023-05-19 19:13:38 -0500

    Added todo to remind myself to switch up the library. For now it is working, but it will not on a future version of Rust. Must find replacement for Soup in the future.

--- src/html_module.rs
+++ src/html_module.rs
@@ -6,7 +6,13 @@ Operating system: Fedora 38/WSL
 Purpose: This class is meant to process and return HTML formatted text as strings.
 Last edited: 5/19/23
  */
-
+//TODO: I received a warning that soup is going to be incompatible in a future version, as it uses an old version of html5ever.
+/*Possible solutions:
+ - Convert to html5ever
+ - find alternative library/framework
+ - convert html module to python script, use python version of soup
+ - convert html by hand(absolutely not)
+ */
 use soup::{NodeExt, QueryBuilderExt, Soup};
 
 pub fn main(content: String) -> String {
